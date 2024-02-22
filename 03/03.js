c2dce2aaf4f90473446a536919af72d2fd9c7f9d
bokuwahanya <los.oplia@gmail.com>
2024-02-22 12:58:46 +0900

    v4

--- 03/03.js
+++ 03/03.js
@@ -1,5 +1,7 @@
 //접근자 없음
 // 자바 스크립트 함수 만들기
+
+/*함수 작성 1
 function handleClick(n) {
     //메세지 영역 가져오기 1
    // document.getElementById("msgArea").innerHTML = "안녕하세요" ;
@@ -12,3 +14,10 @@ function handleClick(n) {
    //백틱 문자열
 
 }
+*/
+
+// 함수 작성 2 : 화살표 함수
+// const : 상수 선언
+// let : 변수 선언
+
+const handleClick = (n) => {document.querySelector("#msgArea").innerHTML = `<h2>버튼  ${n}  <h2>안녕하세요<h2>`;}
